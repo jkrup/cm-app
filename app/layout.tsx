b6a01958c78e0ec806f8d7c34007663ddd6d4768
jkrup <mazlix@gmail.com>
2025-03-18 14:18:39 -0700

    Change color of themebar manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-title" content="Chonky Mammoth" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="theme-color" content="#4A5568" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className="min-h-screen">
